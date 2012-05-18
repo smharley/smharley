2dfaa8a91e38cc9b058981af95536f2a4649c242
Steven Harley <sharley@Mac-Pro.local>
2012-05-18 10:51:59 -0700

    got compass working and added cache and ds store to gitignore

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+css_dir = "/static/styles/css/"
+sass_dir = "/static/styles/sass/"
+images_dir = "/static/images/"
+output_style = :compressed
+relative_assets = true
+line_comments = false
