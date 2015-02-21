3618f882b6a181db277b19d59cb93a31a4a7a0ce
Steven Harley <smharley@gmail.com>
2015-02-20 17:20:17 -0800

    Initial commit. Reboot

--- config.rb
+++
@@ -1,8 +0,0 @@
-# Require any additional compass plugins here.
-project_type = :stand_alone
-css_dir = "/static/styles/css/"
-sass_dir = "/static/styles/sass/"
-images_dir = "/static/images/"
-output_style = :compact
-relative_assets = true
-line_comments = false
